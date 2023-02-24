4bc78e68f7f410a0f0fd538044c6fe0cb3772aa6
Samantha <Troyandme04@gmail.com>
2023-02-24 10:54:35 -0500

    header

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
-//FEH Robot project
+/* =========================================================================
+*                       FEH Robot Project Group 910F
+*
+*Group Members: Searita Chen, Bennett Godinho-Nelson, Samantha Smith, John Ulm
+*
+*                             Robot Code
+===========================================================================*/
 int main() {
 
 }
