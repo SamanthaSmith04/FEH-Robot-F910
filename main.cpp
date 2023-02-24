4bb6ef7b2b46c1dbe23f679b9d02d9ece6538740
Samantha <Troyandme04@gmail.com>
2023-02-24 11:00:59 -0500

    added imports and constants

--- main.cpp
+++ main.cpp
@@ -5,6 +5,26 @@
 *
 *                             Robot Code
 ===========================================================================*/
+
+//IMPORTS
+#include <FEHLCD.h>
+#include <FEHMotor.h>
+#include <FEHServo.h>
+#include <FEHIO.h>
+#include <FEHUtility.h>
+
+//CONSTANTS
+#define LEFT_MOTOR_PORT FEHMotor::Motor0
+#define LEFT_MOTOR_PORT FEHMotor::Motor1
+#define ARM_MOTOR_PORT FEHMotor::Motor2
+#define DROPPER_SERVO_PORT FEHServo::Servo0
+#define CDS_SENSOR_PORT FEHIO::P0_0
+#define BUMP_SWITCH_PORT FEHIO::P0_1
+
+
+//GLOBAL VARIABLES
+
+
 int main() {
 
 }
--- main.d
+++ main.d
@@ -1,16 +1,1 @@
-../main.d: ../main.cpp Libraries/FEHLCD.h Drivers/spi.h \
- ../fehproteusfirmware/Startup/derivative.h Startup/MK60DZ10.h \
- Libraries/FEHUtility.h Startup/derivative.h Libraries/LCDColors.h \
- Libraries/FEHMotor.h Libraries/FEHServo.h Libraries/FEHIO.h \
- Drivers/adc16.h
-Libraries/FEHLCD.h:
-Drivers/spi.h:
-../fehproteusfirmware/Startup/derivative.h:
-Startup/MK60DZ10.h:
-Libraries/FEHUtility.h:
-Startup/derivative.h:
-Libraries/LCDColors.h:
-Libraries/FEHMotor.h:
-Libraries/FEHServo.h:
-Libraries/FEHIO.h:
-Drivers/adc16.h:
+../main.d: ../main.cpp
