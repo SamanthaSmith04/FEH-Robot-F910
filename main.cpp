50dcd5fb0b3c9d127baadfddeb25a2397c3370bc
BennettGN <bennettgodinhonelson@gmail.com>
2023-02-27 09:32:47 -0500

    Move Function

--- main.cpp
+++ main.cpp
@@ -21,9 +21,17 @@
 #define CDS_SENSOR_PORT FEHIO::P0_0
 #define CDS_START_THRESHOLD 0
 #define BUMP_SWITCH_PORT FEHIO::P0_1
+#define CIRCUM (2*3.1415*3)
+
+//Declarations for Encoders, Arbitrary Ports
+DigitalEncoder right_encoder(FEHIO::P0_0);
+DigitalEncoder left_encoder(FEHIO::P0_1);
+FEHMotor right_motor(FEHMotor::Motor0,9.0);
+FEHMotor left_motor(FEHMotor::Motor1,9.0);
+
 //Arbitrary Red Value
 #define RED 1
-#define CIRCUM (2*3.1415*3)
+
 
 
 //GLOBAL VARIABLES
@@ -53,3 +61,31 @@ void testCDS(){
        Sleep(0.1);
    }
 }
+
+//-1 for backwards, 1 for forwards
+void move(int percent, double inches) //using encoders
+{
+
+
+    int counts = (int) ((inches/CIRCUM)*318);
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+
+    //Set both motors to desired percent
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
+
+
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+}
+
+
